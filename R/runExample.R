9330391390a178e68f4a1cb6a4e3e8876af2d10d
mas29 <mas29@sfu.ca>
2016-05-19 10:25:35 -0700

    ExAKHTLI made into an R package

---
+++ R/runExample.R
@@ -0,0 +1,9 @@
+#' @export
+runExample <- function() {
+  appDir <- system.file("shiny", "myapp", package = "ExAKHTLI")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `ExAKHTLI`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
